c1b27b010182ade9033eaf5e45bd03d4a2dabc24
Saimkh32 <moizkh392@gmail.com>
2026-07-09 09:53:44 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,43 @@
+# Variable in python
+a = 1     # integer number
+print (a)
+
+b = 1.1357101      # decimal number
+print (b)
+
+c = "Saim khan"    # word sentence-use quotes
+print (c)
+
+Rishabh = 26     # declare a variable using variables
+d = Rishabh
+print (d)
+
+x = 10     # adding a number to a variable
+print (x+1)
+
+p,q,r = 11,12,13 # assign multiple variables
+print (p,q,r)
+
+# way to declare variable name 
+MyName = "Saim khan"  # pascal case
+myName = "Saim khan"  # camel case
+myname = "saim kahn"  # flat case
+my_name = "saim khan" # snake case
+
+# rule to declare a variable
+# 1. Variable name must start with : letter or _
+_myname_ = "Saim khan"
+print (_myname_)
+myname = "Saim khan"
+
+# 2. Variable name can only contain : letters, numbers,underscore(_)
+my_name1 = "Saim khan"
+r1 = 26
+_1 = 1
+
+# 3. variable are case sensetive
+myname = "Rishabh Mishra"
+print (myname)
+
+# 4. variable name is can't be a reserved word
+# for = 10 = for is a reserved word in python
