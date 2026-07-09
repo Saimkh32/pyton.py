6441bd2f9236d3fea94f8d2dd00f3b9538f26c05
Saim khan <moizkh392@gmail.com>
2026-07-09 15:22:27 +0500

    Add files via upload

--- datatypes.py
+++ datatypes.py
@@ -1,1 +1,47 @@
+# data type in python \
+a = 1
+b = 1
+print(a+b)
+print(type(a)) # checking data type :integer
+
+c = "1"
+d = "1"
+print (c+d)
+print(type(c)) # checking data type :string
+
+# basic data types in python:
+#1. numeric 
+a1 = 1       #1a.integer
+a2 = 1.5     #1b.float
+print(type(a2))
+a3 = complex (3,5) #1c. complex 
+print(type(a3))
+
+# 2.sequence data types
+b1 = "Saim khan" #2a.string
+b11 = '26'
+print(type(b1))
+b2 = [1,4,7,26,108] #2b.list
+print(type(b2))
+b3 = (1,4,7,26,108) #2c.tuple
+print(type(b3))
+
+# 3.Dictionary
+my_dictionary = {'name': 'Saim', 'age': 18, 'city': 'Karachi'}
+print(type(my_dictionary))
+
+# 4. Set
+my_set = {1,4,7,26,108}
+print(type(my_set))
+
+# 5. Boolean
+bool1 = True
+bool2 = False
+print(type(bool1))
+
+ #6. Binary
+# bytes, bytearray, memoryview
+byte1 = b"Rishabh Mishra"
+print(type(byte1))
+
 
